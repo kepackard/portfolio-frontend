7813c70babf5e053e178827b0509cfd1d7fdb8fe
kepackard <kepackard@gmail.com>
2022-01-09 20:25:14 -0500

    Adds new project

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,8 +2,26 @@ function Portfolio(props) {
     return (
         <div className="port-wrapper">
             <h1 className="portfolio">Portfolio</h1>
-            <div className="Project4">
-                <div className="proj-box">
+                <div className="Project5">
+                    <div className="proj-box">
+                        <h2 classname="proj-name">Poem of the Day</h2>
+                        <a href="https://poemoftheday.netlify.app/" target="_blank" rel="noreferrer" alt="app link">Launch Poem of the Day</a>
+                        <p>A new poem each day generated from Poems.One API. HTML, CSS, JS, Jquery, Netlify.</p>
+                        <p>
+                            <table>
+                                <tr><th>Technologies Used:</th></tr>
+                                <tr><td>HTML</td></tr>
+                                <tr><td>CSS</td></tr>
+                                <tr><td>Javascript</td></tr>
+                                <tr><td>JQuery</td></tr>
+                                <tr><td>Netlify</td></tr>
+                            </table>
+                        </p>
+                        <a href="https://github.com/kepackard/poetry-api" target="_blank" rel="noreferrer" alt="app link">Go to GitHub</a>
+                    </div>
+                </div>
+                <div className="Project4">
+                    <div className="proj-box">
                     <h2 classname="proj-name">SupplyLab</h2>
                     <a href="https://supplylab.herokuapp.com/" target="_blank" rel="noreferrer" alt="app link">Launch SupplyLab</a>
                     <p>Django app enabling teachers to crowd-source school supplies. A group project where I was responsible for programming, user stories, image research, bug fixing, and deployment.</p>

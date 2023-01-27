25fd61833ba9ebf9a9b06a9058ba7972220d0b79
kepackard <kepackard@gmail.com>
2023-01-27 12:15:29 -0500

    Replaces resume v. 3

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ function Resume(props) {
     return (
         <div className="resume-wrapper">
             <h1 className="resume">Resume</h1>
-            <img className="resume-img" src="https://i.imgur.com/jpAPKVi.png" alt="resume" target="_blank" rel="noreferrer"/>
+            <img className="resume-img" src="https://i.imgur.com/mnW1vzp.png" alt="resume" target="_blank" rel="noreferrer"/>
         </div>
     )
 }
